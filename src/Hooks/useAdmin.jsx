557a6b41ee55e5026451daa73f2472e5240e8a8c
hasanisokay <hasanisokay@gmail.com>
2023-06-09 00:42:40 +0600

    use admin and use instructor hooks added

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -4,7 +4,7 @@ import useAxiosSecure from './useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 
 const useAdmin = () => {
-    const {user,loading} = useAuth()
+    const {user, loading} = useAuth()
     const [ axiosSecure] = useAxiosSecure()
     const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
         queryKey: ["isAdmin", user?.email],
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import useAdmin from '../../Hooks/useAdmin';
+import useInstructor from '../../Hooks/useInstructor';
 
 const Dashboard = () => {
+    const [isAdmin, isAdminLoading] = useAdmin()
+    const [isInstructor, isInstructorLoading] = useInstructor()
+    console.log("admin", isAdmin);
+    console.log("instructor", isInstructor);
     return (
-        <div className='py-40'>
+        <div className='pt-20'>
               <Helmet>
                 <title>Light & Shadow | Dashboard</title>
             </Helmet>
-            This is dashboard
+            <h1>Th</h1>
             
         </div>
     );
