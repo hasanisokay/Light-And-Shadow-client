4bbd1a7823f9828bffc802a8e37cc406431fc806
hasanisokay <hasanisokay@gmail.com>
2023-06-08 23:40:35 +0600

    added class cards

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import ClassCard from '../Shared/ClassCard/ClassCard';
 
 const Classes = () => {
+    const [classes, setClasses] = useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:5000/classes`)
+        .then(res=>res.json())
+        .then(data=>setClasses(data))
+    },[])
+    console.log(classes);
     return (
         <div>
               <Helmet>
                 <title>Light & Shadow | Classes</title>
             </Helmet>
+            <div className='grid grid-cols-1 overflow-hidden pt-20 mx-6'>
+            {
+               classes && classes.map((singleClass)=><ClassCard key={singleClass._id} singleClass={singleClass}></ClassCard>)
+            }
+            </div>
             
-            classes
         </div>
     );
 };
--- src/Pages/Shared/ClassCard/ClassCard.jsx
+++ src/Pages/Shared/ClassCard/ClassCard.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
-const ClassCard = ({singleClass}) =>{
-    const {available_seats, class_instructor_name, class_title, image_link, price} = singleClass;
+const ClassCard = ({ singleClass }) => {
+    const { available_seats, class_instructor_name, class_title, image_link, price } = singleClass;
 
     return (
         <div>
-            <div className="bg-[#d7d2b7] overflow-hidden hover:scale-105 transition rounded-t-2xl rounded-b-md flex lg:flex-row md:flex-row flex-col items-center my-4">
-                        <figure className='lg:w-[320px] md:w-[300px] h-[250px] w-full lg:h-[300px]'><img src={image_link} className='h-full w-full' alt="class" /></figure>
-                        <div className="mx-4 my-4">
-                            <h2 className="lg:text-2xl text-lg"><span className='font-semibold' >{class_title}</span> </h2>
-                            <h2 className="lg:text-lg text-base">Course Instructor: <span className='font-semibold'>{class_instructor_name}</span></h2>
-                            <h2 className="lg:text-lg text-base">Available Seat: <span className='font-semibold'>{available_seats}</span></h2>
-                            <h2 className="lg:text-lg text-base mb-2 ">Price: <span className='font-semibold'>$ {price}</span></h2>
-                            <button className='bg-[#031003] py-1 px-2 rounded-lg hover:bg-[#111827] text-white cursor-pointer'>Select Course</button>
+            <div className="bg-[#d7d2b7] overflow-hidden hover:scale-105 transition rounded-t-2xl mb-4 rounded-b-md flex lg:flex-row md:flex-row flex-col gap-4 items-center justify-between">
+                <figure className='w-full'>
+                    {/* lg:w-[300px] lg:h-[250px] md:h-[250px] md:w-[200px] w-full h-full */}
+                    <img src={image_link} className='lg:max-w-[400px] lg:min-w-[400px] md:max-w-[300px] md:min-w-[300px] lg:max-h-[280px] lg:min-h-[280px] md:max-h-[300px] md:min-h-[300px] max-w-full min-w-full max-h-[200px] min-h-[200px]' alt="class" />
+                </figure>
+                <div className="mx-4 lg:ms-2 lg:w-full md:w-[99%] md:mx-8">
+                    <h2 className="lg:text-2xl text-lg"><span className='font-semibold' >{class_title}</span> </h2>
+                    <h2 className="lg:text-lg text-base">Course Instructor: <span className='font-semibold'>{class_instructor_name}</span></h2>
+                    <h2 className="lg:text-lg text-base">Available Seat: <span className='font-semibold'>{available_seats}</span></h2>
+                    <h2 className="lg:text-lg text-base mb-2 ">Price: <span className='font-semibold'>$ {price}</span></h2>
+                    <button className='bg-[#031003] py-1 px-2 rounded-lg hover:bg-[#111827] text-white cursor-pointer'>Select Course</button>
+                </div>
+            </div>
 
-                        </div>
-                    </div>
-            
         </div>
     );
 };
