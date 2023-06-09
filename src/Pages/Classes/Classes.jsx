67f0cdf8ab103572818959b29b9dff5b5b22252b
hasanisokay <hasanisokay@gmail.com>
2023-06-09 20:13:27 +0600

    popular classes added

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -13,11 +13,13 @@ const Classes = () => {
     },[])
     console.log(classes);
     return (
-        <div>
+        <div className='mx-6'>
               <Helmet>
                 <title>Light & Shadow | Classes</title>
             </Helmet>
-            <div className='grid grid-cols-1 overflow-hidden pt-20 mx-6'>
+            <h1 className='font-semibold font-poppins text-white pt-20 text-center text-xl lg:text-3xl'>Browse and Enroll in our Exciting Photography Classes</h1>
+            <p className='text-white font-roboto mb-4'>Welcome to our comprehensive classes page, where you can explore a wide variety of captivating photography courses tailored to suit your interests and skill level. From mastering the fundamentals to exploring advanced techniques, our classes are designed to empower you with the knowledge and skills needed to elevate your photography game. With a diverse range of topics, including landscape photography, portrait photography, photo editing, and more, there's something for every aspiring photographer. Take the next step in your photographic journey and enroll in a class today to unlock your creative potential and capture stunning moments with confidence.</p>
+            <div className='grid grid-cols-1 overflow-hidden'>
             {
                classes && classes.map((singleClass)=><ClassCard key={singleClass._id} singleClass={singleClass}></ClassCard>)
             }
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import bg from "../../../assets/images/gallery-bg.png"
+import ClassCard from '../../Shared/ClassCard/ClassCard';
 const PopularClasses = () => {
     const [popularClasses, setPopularClasses] = useState([])
     useEffect(()=>{
@@ -11,14 +12,16 @@ const PopularClasses = () => {
         .then(data=>setPopularClasses(data.data))
     },[])
     return (
-        <div className='mb-10'>
-           <div className='text-white lg:w-[50%] mb-12 mx-6' >
-           <h1 className='font-poppins lg:text-3xl text-xl mt-2'>Popular Classes to Elevate Your Photography Skills</h1>
+        <div className='mb-10 mx-6'>
+           <div className='text-white mb-4 ' >
+           <h1 className='font-poppins lg:text-3xl text-xl mt-2 text-center'>Popular Classes to Elevate Your Photography Skills</h1>
             <p className='mt-1'>Explore our curated selection of popular classes that have captivated aspiring photographers around the world. Whether you're a beginner eager to learn the fundamentals or an experienced shutterbug looking to expand your repertoire, we have the perfect class to suit your needs. Enroll in one or more of these popular classes and join a community of passionate photographers who are dedicated to honing their craft. Unleash your creativity, expand your technical knowledge, and embark on a transformative journey that will elevate your photography skills to new heights.</p>
             <Link to="/classes"><button className='my-btn my-4'>See All Classes</button></Link>
            </div>
-           <div>
-            <div><img src={bg} className='transform -rotate-45 h-full w-[90%]' alt="" /></div>
+           <div className='overflow-hidden'>
+            {
+                popularClasses.map( singleClass=><ClassCard singleClass={singleClass}></ClassCard>)
+            }
            </div>
         </div>
     );
